e3d7e0fe663c0fe8028ff3a68efd694e36c39cf3
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-16 22:17:45 +0200

    split

--- manydepth/options.py
+++ manydepth/options.py
@@ -216,7 +216,7 @@ class MonodepthOptions:
                                  type=str,
                                  default="eigen",
                                  choices=["eigen", "eigen_benchmark", "benchmark", "odom_9",
-                                          "odom_10", "cityscapes",,"endovis"],
+                                          "odom_10", "cityscapes","endovis"],
                                  help="which split to run eval on")
         self.parser.add_argument("--save_pred_disps",
                                  help="if set saves predicted disparities",
