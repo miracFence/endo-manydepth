044cc7ae5d8a28b8e59c3961888008aa263ff423
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-16 22:13:54 +0200

    run

--- manydepth/datasets/kitti_dataset.py
+++ manydepth/datasets/kitti_dataset.py
@@ -12,8 +12,8 @@ import skimage.transform
 import numpy as np
 import PIL.Image as pil
 
-from manydepth.kitti_utils import generate_depth_map
-from .mono_dataset import MonoDataset
+from kitti_utils import generate_depth_map
+from mono_dataset import MonoDataset
 
 
 class KITTIDataset(MonoDataset):
