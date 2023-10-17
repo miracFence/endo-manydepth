07488e965373efb055fd31e426390ae94fdcabb1
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-17 23:43:21 +0200

    wandb

--- manydepth/trainer.py
+++ manydepth/trainer.py
@@ -772,7 +772,7 @@ class Trainer:
                 
                 if s == 0 and frame_id != 0:
                     wandb.log({"color_pred_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs[("color", frame_id, s)][j].data)},step=self.step)
-                    wandb.log({"color_pred_refined_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(scale)][j].data)},step=self.step)
+                    wandb.log({"color_pred_refined_{}_{}/{}".format(frame_id, s, j): wandb.Image(outputs["refinedCB_"+str(frame_id)+"_"+str(s)][j].data)},step=self.step)
             disp = colormap(outputs[("disp", s)][j, 0])
             wandb.log({"disp_multi_{}/{}".format(s, j): wandb.Image(disp.transpose(1, 2, 0))},step=self.step)
             disp = colormap(outputs[('mono_disp', s)][j, 0])
