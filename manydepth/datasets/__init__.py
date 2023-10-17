a74cd4c081bc8bc499ab8ec78b6224ca64f81650
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-17 14:45:07 +0200

    from .scared_dataset import SCAREDDataset

--- manydepth/datasets/__init__.py
+++ manydepth/datasets/__init__.py
@@ -3,3 +3,4 @@ from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
 from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
 from .cityscapes_evaldataset import CityscapesEvalDataset
 from .scared_dataset import SCAREDDataset
+from .scared_dataset import SCAREDRAWDataset
