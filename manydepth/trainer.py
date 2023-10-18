1da31e66c9ae869d41907c381e68a84403233c33
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-18 12:24:09 +0200

    no grads

--- manydepth/trainer.py
+++ manydepth/trainer.py
@@ -457,12 +457,13 @@ class Trainer:
             # now we need poses for matching - compute without gradients
             pose_feats = {f_i: inputs["color_aug", f_i, 0] for f_i in self.matching_ids}
             
-            for f_i in self.matching_ids:
-                if f_i != 0:
-                    pose_feats[f_i] = outputs["c_"+str(f_i)+"_"+str(0)] * pose_feats[f_i] + outputs["b_"+str(f_i)+"_"+str(0)]
 
             #outputs["refinedCB_"+str(frame_id)+"_"+str(scale)] = outputs["ch_"+str(frame_id)+"_"+str(scale)] * outputs[("color", frame_id, scale)]  + outputs["bh_"+str(frame_id)+"_"+str(scale)]
             with torch.no_grad():
+                # Lighting change 
+            for f_i in self.matching_ids:
+                if f_i != 0:
+                    pose_feats[f_i] = outputs["c_"+str(f_i)+"_"+str(0)] * pose_feats[f_i] + outputs["b_"+str(f_i)+"_"+str(0)]
                 # compute pose from 0->-1, -1->-2, -2->-3 etc and multiply to find 0->-3
                 for fi in self.matching_ids[1:]:
                     if fi < 0:
