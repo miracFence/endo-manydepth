25f9a8b73745f32efeed433e6f57f4399def98ce
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-24 13:54:28 +0200

    model_lr_scheduler

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -215,7 +215,7 @@ class Trainer_Monodepth:
     def run_epoch(self):
         """Run a single epoch of training and validation
         """
-        self.model_lr_scheduler.step()
+        
 
         print("Training")
         self.set_train()
@@ -246,7 +246,7 @@ class Trainer_Monodepth:
                 self.val()
 
             self.step += 1
-
+        self.model_lr_scheduler.step()
     def process_batch(self, inputs):
         """Pass a minibatch through the network and generate images and losses
         """
