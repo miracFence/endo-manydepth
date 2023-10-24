fbb9a1d9d9a6503018cc8ea6720b6118c26ae4e1
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-24 17:50:25 +0200

    ident

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -332,10 +332,10 @@ class Trainer_Monodepth:
             for f_i in self.opt.frame_ids[1:]:
                 for scale in self.opt.scales:
                     outputs[("bh",scale, f_i)] = F.interpolate(outputs["b_"+str(scale)+"_"+str(f_i)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
-                        outputs[("ch",scale, f_i)] = F.interpolate(outputs["c_"+str(scale)+"_"+str(f_i)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
-                        input_original = inputs[("color", 0, 0)].detach()
-                        outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, f_i)] * input_original + outputs[("bh", scale, f_i)]
-                        
+                    outputs[("ch",scale, f_i)] = F.interpolate(outputs["c_"+str(scale)+"_"+str(f_i)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
+                    input_original = inputs[("color", 0, 0)].detach()
+                    outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, f_i)] * input_original + outputs[("bh", scale, f_i)]
+
         else:
             # Here we input all frames to the pose net (and predict all poses) together
             if self.opt.pose_model_type in ["separate_resnet", "posecnn"]:
