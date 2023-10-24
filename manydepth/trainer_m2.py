bcb4763dbac5094728f4665b20e7e2f074ed456e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-24 13:36:44 +0200

    indentations

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -680,31 +680,31 @@ class Trainer_Monodepth:
             print("Cannot find Adam weights so Adam is randomly initialized")
     
     def colormap(inputs, normalize=True, torch_transpose=True):
-    if isinstance(inputs, torch.Tensor):
-        inputs = inputs.detach().cpu().numpy()
+        if isinstance(inputs, torch.Tensor):
+            inputs = inputs.detach().cpu().numpy()
 
-    vis = inputs
-    if normalize:
-        ma = float(vis.max())
-        mi = float(vis.min())
-        d = ma - mi if ma != mi else 1e5
-        vis = (vis - mi) / d
+        vis = inputs
+        if normalize:
+            ma = float(vis.max())
+            mi = float(vis.min())
+            d = ma - mi if ma != mi else 1e5
+            vis = (vis - mi) / d
 
-    if vis.ndim == 4:
-        vis = vis.transpose([0, 2, 3, 1])
-        vis = _DEPTH_COLORMAP(vis)
-        vis = vis[:, :, :, 0, :3]
-        if torch_transpose:
-            vis = vis.transpose(0, 3, 1, 2)
-    elif vis.ndim == 3:
-        vis = _DEPTH_COLORMAP(vis)
-        vis = vis[:, :, :, :3]
-        if torch_transpose:
-            vis = vis.transpose(0, 3, 1, 2)
-    elif vis.ndim == 2:
-        vis = _DEPTH_COLORMAP(vis)
-        vis = vis[..., :3]
-        if torch_transpose:
-            vis = vis.transpose(2, 0, 1)
+        if vis.ndim == 4:
+            vis = vis.transpose([0, 2, 3, 1])
+            vis = _DEPTH_COLORMAP(vis)
+            vis = vis[:, :, :, 0, :3]
+            if torch_transpose:
+                vis = vis.transpose(0, 3, 1, 2)
+        elif vis.ndim == 3:
+            vis = _DEPTH_COLORMAP(vis)
+            vis = vis[:, :, :, :3]
+            if torch_transpose:
+                vis = vis.transpose(0, 3, 1, 2)
+        elif vis.ndim == 2:
+            vis = _DEPTH_COLORMAP(vis)
+            vis = vis[..., :3]
+            if torch_transpose:
+                vis = vis.transpose(2, 0, 1)
 
-    return vis
+        return vis
