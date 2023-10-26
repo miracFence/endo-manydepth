9f4a1be586103a82d7a79218d7165f86d2a1d2ce
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-26 14:10:10 +0200

    ResnetEncoderIIF

--- manydepth/networks/encoder_iif.py
+++ manydepth/networks/encoder_iif.py
@@ -57,7 +57,7 @@ class ResnetEncoderIIF(nn.Module):
     """Pytorch module for a resnet encoder
     """
     def __init__(self, num_layers, pretrained, num_input_images=1):
-        super(ResnetEncoderIIL, self).__init__()
+        super(ResnetEncoderIIF, self).__init__()
 
         self.num_ch_enc = np.array([64, 64, 128, 256, 512])
 
