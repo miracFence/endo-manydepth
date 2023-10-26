21e659dc82926a757e037a8ca6397b6f6515a789
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-26 14:17:54 +0200

    self.

--- manydepth/networks/pose_decoder.py
+++ manydepth/networks/pose_decoder.py
@@ -34,7 +34,7 @@ class PoseDecoder(nn.Module):
 
     def forward(self, input_features):
         last_features = [f[-1] for f in input_features]
-        print(num_input_features)
+        print(self.num_input_features)
         cat_features = [self.relu(self.convs["squeeze"](f)) for f in last_features]
         cat_features = torch.cat(cat_features, 1)
 
