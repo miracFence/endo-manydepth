a38cfe7fbb02d09bbdc840f7c18046a5c8084a94
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-26 17:51:09 +0200

    motion_inputs

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -353,7 +353,7 @@ class Trainer_Monodepth:
                         axisangle[:, 0], translation[:, 0])
                     
                     outputs_lighting = self.models["lighting"](pose_inputs[0])
-                    outputs_mf = self.models["motion_flow"](pose_inputs[0])
+                    outputs_mf = self.models["motion_flow"](motion_inputs[0])
 
                     for scale in self.opt.scales:
                         outputs["b_"+str(scale)+"_"+str(f_i)] = outputs_lighting[("lighting", scale)][:,0,None,:, :]
