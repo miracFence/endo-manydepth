88d968adbc0ee250688f943c281fb462fcfd6d68
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-27 12:50:11 +0200

    ssim_loss

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -552,8 +552,7 @@ class Trainer_Monodepth:
                 pred = outputs[("color", frame_id, scale)]
                 loss_reprojection += (self.compute_reprojection_loss(pred, target) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
                 target = inputs[("color", 0, 0)]
-                loss_ilumination_invariant += (
-                    self.get_ilumination_invariant_loss(pred,target))
+                loss_ilumination_invariant += (self.get_ilumination_invariant_loss(pred,target))
             
             loss += loss_reprojection / 2.0
             print(loss_ilumination_invariant)
