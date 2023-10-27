a710e4b5b944a7ad234e3af6e6eb15eb15eca036
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-27 13:22:53 +0200

    loss_ilumination_invariant

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -556,7 +556,7 @@ class Trainer_Monodepth:
             
             loss += loss_reprojection / 2.0
             #print(loss_ilumination_invariant)
-            loss += loss_ilumination_invariant / 2.0
+            loss += 0.5 * loss_ilumination_invariant / 2.0
             mean_disp = disp.mean(2, True).mean(3, True)
             norm_disp = disp / (mean_disp + 1e-7)
             smooth_loss = get_smooth_loss(norm_disp, color)
