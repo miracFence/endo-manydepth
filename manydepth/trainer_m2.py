548012cdb98be207b15b4db45e97501f090215d4
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-10-27 12:07:57 +0200

    ssim_loss

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -510,13 +510,13 @@ class Trainer_Monodepth:
     def get_ilumination_invariant_loss(self, pred, target):
         features_p = get_ilumination_invariant_features(pred)
         features_t = get_ilumination_invariant_features(target)
-        abs_diff = torch.abs(features_t - features_p)
-        l1_loss = abs_diff.mean(1, True)
+        """abs_diff = torch.abs(features_t - features_p)
+        l1_loss = abs_diff.mean(1, True)"""
 
         ssim_loss = self.ssim(features_p, features_t).mean(1, True)
-        ii_loss = 0.85 * ssim_loss + 0.15 * l1_loss
+        #ii_loss = 0.85 * ssim_loss + 0.15 * l1_loss
 
-        return ii_loss
+        return ssim_loss
     
     def compute_losses(self, inputs, outputs):
 
