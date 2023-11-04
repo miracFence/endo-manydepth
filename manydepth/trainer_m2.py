601a518465e9373f778b6186da2705523a5abaa8
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-11-04 13:00:50 +0100

    pa = torch.tensor(pa)

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -562,8 +562,9 @@ class Trainer_Monodepth:
     def get_v(self,depth_data,pa,pb,K):
         pa_y,pa_x = pa
         pb_y,pb_x = pb
-        pa = torch.stack(list(pa), dim=0)
-        pb = torch.stack(list(pb), dim=0)
+        pa = torch.tensor(pa)
+        pb = torch.tensor(pb)
+        
         #print(pa)
         #print(pb)
         #print(pa_y)
