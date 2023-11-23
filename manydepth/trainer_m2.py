95ce07c6525e3a31943cb200e111222e694d00e9
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-11-22 20:54:40 -0600

    X_tilde_p

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -580,9 +580,9 @@ class Trainer_Monodepth:
                     X_tilde_p = torch.matmul(K_inv[b][:3,:3], p)
 
                     # Calculate dot products
-                    #print(N_hat[b, i, j].shape)
-                    #print(X_tilde_p.shape)
-                    cpp = N_hat[b, i, j] * X_tilde_p
+                    print(N_hat.shape)
+                    print(X_tilde_p.shape)
+                    cpp = torch.dot(N_hat[b, i, j],X_tilde_p)
                     
                     # Iterate over neighboring pixels
                     for neighbor_offset in [(0, 1), (1, 0)]:
@@ -593,7 +593,7 @@ class Trainer_Monodepth:
                         X_tilde_q = torch.matmul(K_inv[b][:3,:3], q)
 
                         # Calculate dot products
-                        cpq = N_hat[b ,i, j]* X_tilde_q
+                        cpq = torch.dot(N_hat[b ,i, j].squeeze(), X_tilde_q)
 
                         # Update LDN loss
                         LDN_loss += torch.abs(D[i, j] * cpq - D[ni, nj] * cpp)
