0c2cf2f1bdf7a1d386c6dd3970a6a0d6abbb5a8d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-11-23 16:26:01 -0600

    normal_loss

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -631,9 +631,11 @@ class Trainer_Monodepth:
                 #target = inputs[("color", 0, 0)]
                 #loss_ilumination_invariant += (self.get_ilumination_invariant_loss(pred,target) * reprojection_loss_mask_iil).sum() / reprojection_loss_mask_iil.sum()
                 #Normal loss
-                #normal_loss += (self.norm_loss(outputs[("normal",frame_id)][("normal", 0)],outputs["normal_inputs"][("normal", 0)], rot_from_axisangle(outputs[("axisangle", 0, frame_id)][:, 0]),frame_id) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
+                normal_loss += (self.norm_loss(outputs[("normal",frame_id)][("normal", 0)],outputs["normal_inputs"][("normal", 0)], rot_from_axisangle(outputs[("axisangle", 0, frame_id)][:, 0]),frame_id) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
                 
                 #total_loss += orthonogal_loss
+            #Normal loss
+            loss += 0.005 * normal_loss
             #Orthogonal loss
             loss += 0.001 * self.compute_ldn_loss(outputs[("disp", scale)], outputs["normal_inputs"][("normal", scale)], inputs[("inv_K", scale)].detach())
             loss += loss_reprojection / 2.0
