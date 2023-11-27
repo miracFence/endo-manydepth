8356ed6a1091db886afcf5e00b0457deade1d8c6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-11-27 12:50:46 -0600

    normal_decoder

--- manydepth/networks/__init__.py
+++ manydepth/networks/__init__.py
@@ -1,9 +1,6 @@
-# flake8: noqa: F401
 from .resnet_encoder import ResnetEncoder, ResnetEncoderMatching
 from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .lighting_decoder import LightingDecoder
 from .normal_decoder import NormalDecoder
-#from .residual_flow_decoder import ResidualFLowDecoder
-#from .encoder_iif import ResnetEncoderIIF
