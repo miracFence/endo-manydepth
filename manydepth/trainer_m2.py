fd91db0a8ac3f9d4f8d3f5ca5de8238ccca83c30
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-01 18:49:57 -0600

    compute_orth_loss

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -639,8 +639,7 @@ class Trainer_Monodepth:
         Cpp = torch.einsum('bijk,bijk->bij', N_hat, X_tilde_p)
         
         for p_idx in [p1, p2, p3, p4]:
-            q = P.roll(p_idx.item(), -1, dims=-1)
-            q = q[:, :, :2]  # Keep only the first two dimensions
+            q = P.roll(p_idx.item(), -1, dims=-1)[:, :, :2]  # Keep only the first two dimensions
             X_tilde_q = torch.matmul(K_inv[:, :3, :3].unsqueeze(1), q.permute(0, 3, 1, 2).unsqueeze(-1)).squeeze(-1).permute(0, 2, 3, 1)
             Cpq = torch.einsum('bijk,bijk->bij', N_hat, X_tilde_q)
 
@@ -650,7 +649,6 @@ class Trainer_Monodepth:
 
         print(orth_loss)
         return orth_loss
-
 
 
     
