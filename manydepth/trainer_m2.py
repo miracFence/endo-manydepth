4ff69c64f22b31346558d15e615f41a4d946dd9b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-03 13:07:22 -0600

    print(p_idx.item())

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -657,6 +657,8 @@ class Trainer_Monodepth:
         print(Cpp)
         
         for p_idx in [p1, p2, p3, p4]:
+            print(p_idx.item())
+            print(p_idx)
             q = P.roll(p_idx.item(), -1, dims=-1)  # Keep only the first two dimensions
             print(q)
             X_tilde_q = torch.matmul(K_inv[:, :3, :3].unsqueeze(1), q.permute(0, 3, 1, 2).unsqueeze(-1)).squeeze(-1).permute(0, 2, 3, 1)
