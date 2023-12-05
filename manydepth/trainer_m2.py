95828414232477e0379a769e789e27af74f578f5
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-04 18:14:26 -0600

    permute

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -546,6 +546,7 @@ class Trainer_Monodepth:
         #print(rotation_matrix.unsqueeze(1).shape)
         #print(target.shape)
         #print(rotation_matrix.shape)
+        print(target.permute(0, 3, 1, 2).shape)
         rotated_images = torch.matmul(target.permute(0, 3, 1, 2).view(batch_size,channels,-1), rotation_matrix[:, :3, :3]) 
         #print(rotated_images.shape)
         # Reshape the rotated images back to the original shape (12, 3, 256, 320)
