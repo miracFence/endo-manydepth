cba240552595a4c756b5058155feb21d23441cfb
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-04 21:10:46 -0600

    loss_reprojection

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -670,10 +670,10 @@ class Trainer_Monodepth:
             loss += loss_reprojection / 2.0    
             #Normal loss
             #if self.normal_flag == 1:
-            loss += normal_loss / 2.0
+            loss += 0.1 * normal_loss / 2.0
             #Orthogonal loss
             #if self.normal_flag == 1:
-            loss += self.compute_orth_loss(outputs[("disp", scale)], outputs["normal_inputs"][("normal", scale)], inputs[("inv_K", scale)].detach())
+            loss += 0.5 * self.compute_orth_loss(outputs[("disp", scale)], outputs["normal_inputs"][("normal", scale)], inputs[("inv_K", scale)].detach())
             #Illumination invariant loss
             #loss += self.opt.illumination_invariant * loss_ilumination_invariant / 2.0
             mean_disp = disp.mean(2, True).mean(3, True)
