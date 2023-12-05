f0182cbb2b9c235ad44d8044adc18a278aad1116
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-05 14:16:48 -0600

    epoch

--- manydepth/options.py
+++ manydepth/options.py
@@ -114,7 +114,7 @@ class MonodepthOptions:
         self.parser.add_argument("--num_epochs",
                                  type=int,
                                  help="number of epochs",
-                                 default=15)
+                                 default=20)
         self.parser.add_argument("--scheduler_step_size",
                                  type=int,
                                  help="step size of the scheduler",
