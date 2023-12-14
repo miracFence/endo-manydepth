41206c006afc2ca3c4a32aab155cd8bfdf442f7d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-14 10:03:22 -0600

    ref_img

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -572,15 +572,17 @@ class Trainer_Monodepth2:
         #p4 = (2,0)
  
         # Homogeneous coordinates
+        """
         p = torch.arange(height, dtype=torch.float32).view(1, height, 1).to(device=K_inv.device)
         q = torch.arange(width, dtype=torch.float32).view(1, 1, width).to(device=K_inv.device)
      
         p = p.expand(batch_size, height, width).unsqueeze(-1)
         q = q.expand(batch_size, height, width).unsqueeze(-1)
         
-        P = torch.cat([p, q, torch.ones_like(p)], dim=-1)
-        print(P.shape)
-        print(ref_img.shape)
+        P = torch.cat([p, q, torch.ones_like(p)], dim=-1)"""
+        #print(P.shape)
+        #print(ref_img.shape)
+        P = ref_img.permute(0, 2, 3, 1)
         X_tilde_p = torch.matmul(K_inv[:, :3, :3], P.permute(0,3,1,2).view(batch_size,3,-1))
 
         Cpp = torch.einsum('bijk,bijk->bij', N_hat, X_tilde_p.view(batch_size,3,height, width).permute(0,2,3,1))
