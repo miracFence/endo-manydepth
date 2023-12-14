81b0e6591fb112c3ff1b2b428a93ecbf16bb1224
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-14 10:40:04 -0600

    compute_orth_loss2

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -614,7 +614,7 @@ class Trainer_Monodepth2:
         #print(N_hat.shape)
         N_hat = torch.nn.functional.normalize(N_hat, dim=-1)
         #print(D[0,:3,:3])
-        D = torch.nn.functional.normalize(D, dim=-1)
+        #D = torch.nn.functional.normalize(D, dim=-1)
         #print(N_hat[0,:3,:3])
         batch_size, height, width, channels = D.shape
  
