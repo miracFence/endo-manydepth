4af4f71baf32f582654cf37e36630bb4e6218c29
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-14 15:25:34 -0600

    grid_sample

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -537,7 +537,7 @@ class Trainer_Monodepth2:
         # Compute orthogonality loss
         orth_loss = 0.0
         
-        D_inv = 1.0 / D
+        D_inv = 1.0 / D.permute(0, 2, 3, 1)
         N_hat = N_hat.permute(0, 2, 3, 1)
         N_hat = torch.nn.functional.normalize(N_hat, dim=-1)
         #print(N_hat[0,:3,:3])
@@ -561,7 +561,7 @@ class Trainer_Monodepth2:
         Cpp = torch.einsum('bijk,bijk->bij', N_hat, X_tilde_p.view(batch_size,3,height, width).permute(0,2,3,1))
         print(D_inv.shape)
         print(P.shape)
-        D_inv_p = F.grid_sample(D_inv, P.unsqueeze(1))
+        D_inv_p = F.grid_sample(D_inv.permute(0,3,1,2), P.permute(0,3,1,2)[:,:,:,:2])
         #print(P.shape)
         for idx,p_idx in enumerate([-1,-2,-1,-2]):
             if idx < 2:
