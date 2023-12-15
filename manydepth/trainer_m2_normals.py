2a3edbfefe8427e31536107ace651091cf8a49d5
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-14 21:31:02 -0600

    D_inv_p

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -565,6 +565,7 @@ class Trainer_Monodepth2:
         #print(P.shape)
         D_inv_p = F.grid_sample(D_inv, P.permute(0,3,1,2)[:,:,:,:2]).view(batch_size, 1, height, width, 3).squeeze(-1)
         #sampled_image = sampled_image.view(batch_size, 1, height, width, 3).squeeze(-1)
+        print("D_inv_p")
         print(D_inv_p.shape)
         for idx,p_idx in enumerate([-1,-2,-1,-2]):
             if idx < 2:
@@ -580,6 +581,7 @@ class Trainer_Monodepth2:
             #print(P[0,0].shape)
             #print(qq[:,:,:,:2].shape)
             D_inv_q = F.grid_sample(D_inv, qq.permute(0,3,1,2)[:,:,:,:2]).view(batch_size, 1, height, width, 3).squeeze(-1)
+            print("D_inv_q")
             print(D_inv_q.shape)
             Cpq = torch.einsum('bijk,bijk->bij', N_hat, X_tilde_q.view(batch_size,3,height, width).permute(0,2,3,1))
             orth_loss += torch.abs(D_inv_p * torch.unsqueeze(Cpq,0).permute(1,2,3,0) - D_inv_q * torch.unsqueeze(Cpp,0).permute(1,2,3,0))
