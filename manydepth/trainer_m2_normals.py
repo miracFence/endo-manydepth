1d456a300bef4416aa06427490a00f8e8583c0b7
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-17 23:05:10 -0600

    pix_coords

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -605,7 +605,8 @@ class Trainer_Monodepth2:
         pix_coords = pix_coords.repeat(batch_size, 1, 1)
         pix_coords = torch.cat([pix_coords, ones], 1).permute(0,2,1)
         print(pix_coords.shape)
-        print(pix_coords[0,:10])
+        print(pix_coords)
+        #print(pix_coords[0,:10])
         #pa_tl = torch.roll(pix_coords, shifts=1, dims=1)
         #pa_tl = torch.roll(pa_tl, shifts=1, dims=2)
     
