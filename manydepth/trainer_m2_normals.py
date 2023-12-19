0929c217007e104fc5527c7c7ad0d0c62047b9cf
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 11:36:13 -0600

    indexing

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -636,11 +636,11 @@ class Trainer_Monodepth2:
         pa = torch.matmul(K_inv[:, :3, :3],ps["patl"].to(device=K_inv.device))
         pb = torch.matmul(K_inv[:, :3, :3],ps["pbbr"].to(device=K_inv.device))
         
-        ps["patl"] = ps["patl"].view(batch_size, 3, height, width).long()
-        ps["pbbr"] = ps["pbbr"].view(batch_size, 3, height, width).long()
-        ps["patr"] = ps["patr"].view(batch_size, 3, height, width).long()
-        ps["pbbl"] = ps["pbbl"].view(batch_size, 3, height, width).long()
-        print(D[ps["patl"][:,0,:,:],ps["patl"][:,1,:,:]].shape)
+        ps["patl"] = ps["patl"].view(batch_size, height, width,3).long()
+        ps["pbbr"] = ps["pbbr"].view(batch_size, height, width,3).long()
+        ps["patr"] = ps["patr"].view(batch_size, height, width,3).long()
+        ps["pbbl"] = ps["pbbl"].view(batch_size, height, width,3).long()
+        print(D[ps["patl"][:,:,:,0],ps["patl"][:,:,:,1]].shape)
         print(ps["patl"].shape)
 
         V = D[ps["patl"][:,0,:,:],ps["patl"][:,1,:,:]].view(batch_size, 1, -1) * pa - D[ps["pbbr"][:,0,:,:],ps["pbbr"][:,1,:,:]].view(batch_size, 1, -1) * pb        
