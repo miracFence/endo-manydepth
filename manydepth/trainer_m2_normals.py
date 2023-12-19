5576b77c67700721bbe6a23db5f20bbd2309aa6e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 10:06:13 -0600

    K_inv

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -630,7 +630,7 @@ class Trainer_Monodepth2:
             pix_coords = pix_coords.repeat(batch_size, 1, 1)
             pix_coords = torch.cat([pix_coords, ones], 1)
             #print(p_names[idx])
-            ps[p_names[idx]] = pix_coords
+            ps[p_names[idx]] = pix_coords.to(device=K_inv.device)
 
         
         #pa_tr = pix_coords[:,0]-1
