96099e12351cf2af36bac680d19bbbbf57aa31aa
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 19:29:23 -0600

    patl

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -636,6 +636,7 @@ class Trainer_Monodepth2:
         pa = torch.matmul(K_inv[:, :3, :3],ps["patl"].to(device=K_inv.device))
         pb = torch.matmul(K_inv[:, :3, :3],ps["pbbr"].to(device=K_inv.device))
         #ps -> torch.Size([12, 81920, 3])
+        print(ps["patl"].shape)
         ps["patl"] = torch.cat([ps["patl"][:,:,1],ps["patl"][:,:,0]], 1)
         print(ps["patl"].shape)
         generated_depth = F.grid_sample(D,ps["patl"][:,-1:,:].view(12,height, width,2).to(device=K_inv.device),padding_mode="border",align_corners=True)
