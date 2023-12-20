345ee3a94eaade159257614723cd81dbf8f62ad7
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 03:43:43 -0600

    orth_loss

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -749,7 +749,7 @@ class Trainer_Monodepth2:
         #print(V.shape)
 
         
-        orth_loss = torch.einsum('bij,bijk->bik', N_hat.view(batch_size,3,-1), V)
+        orth_loss = torch.einsum('bij,bjkl->bikl', V,N_hat.view(batch_size,3,-1))
         return orth_loss.sum()
 
     
