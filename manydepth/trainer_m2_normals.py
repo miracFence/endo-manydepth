e67a945d860891b85a4879af734f3a6269c70c0d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 19:19:33 -0600

    grid_sample

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -641,7 +641,7 @@ class Trainer_Monodepth2:
         #ps -> torch.Size([12, 81920, 3])
         ps["patl"] = torch.cat([ps["patl"][:,:,1],ps["patl"][:,:,0]])
 
-        generated_depth = F.grid_sample(D,ps["patl"][:,,:].view(12,height, width,2).to(device=K_inv.device),padding_mode="border",align_corners=True)
+        generated_depth = F.grid_sample(D,ps["patl"][:,:2,:].view(12,height, width,2).to(device=K_inv.device),padding_mode="border",align_corners=True)
         print(generated_depth.shape)
         
         #ps["patl"] = ps["patl"].view(batch_size, height, width,3).long()
