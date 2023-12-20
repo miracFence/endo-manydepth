ce7598b393e0dc79b4afac586b21de99e5007c43
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 20:28:24 -0600

    abs

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -651,19 +651,16 @@ class Trainer_Monodepth2:
         pa = torch.matmul(K_inv[:, :3, :3],ps["patl"].to(device=K_inv.device))
         pb = torch.matmul(K_inv[:, :3, :3],ps["pbbr"].to(device=K_inv.device))
         
-        V = Ds["Da_tl"].view(batch_size, 1, -1) * pa - Ds["Db_br"].view(batch_size, 1, -1) * pb
-        orth = torch.einsum('bijk,bijk->bij', N_hat, V.view(batch_size,3,height, width))
-        orth_loss = orth.sum()
+        V = torch.abs(Ds["Da_tl"].view(batch_size, 1, -1) * pa - Ds["Db_br"].view(batch_size, 1, -1) * pb)
         
         pa = torch.matmul(K_inv[:, :3, :3],ps["patr"].to(device=K_inv.device))
         pb = torch.matmul(K_inv[:, :3, :3],ps["pbbl"].to(device=K_inv.device))
 
-        V += Ds["Da_tr"].view(batch_size, 1, -1) * pa - Ds["Db_bl"].view(batch_size, 1, -1) * pb
+        V += torch.abs(Ds["Da_tr"].view(batch_size, 1, -1) * pa - Ds["Db_bl"].view(batch_size, 1, -1) * pb)
 
-        orth = torch.einsum('bijk,bijk->bij', N_hat, V.view(batch_size,3,height, width))
-        orth_loss += orth.sum()
+        orth_loss = torch.einsum('bijk,bijk->bij', N_hat, V.view(batch_size,3,height, width))
         
-        return orth_loss
+        return orth_loss.sum()
 
 
     
