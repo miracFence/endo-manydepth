fa84da8ee9d45c7aa9856a039fba62d575678ce6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-19 18:59:34 -0600

    Dpa

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -659,8 +659,9 @@ class Trainer_Monodepth2:
         #Dpb = D[:,ps["patl"][0,:,1].long(),ps["patl"][0,:,0].long()]
         #print(Dpb.shape)
         #wandb.log({"Dpb": wandb.Image(Dpb[0].permute(2,0,1))},step=self.step)
-        #Dpa = D[:,ps["patl"][0,:,1].long(),ps["patl"][0,:,0].long()]
-        #Dpb = D[:,ps["pbbr"][0,:,1].long(),ps["pbbr"][0,:,0].long()]
+        Dpa = D[:,ps["patl"][0,:,1].long(),ps["patl"][0,:,0].long()]
+        print(Dpa.shape)
+        Dpb = D[:,ps["pbbr"][0,:,1].long(),ps["pbbr"][0,:,0].long()]
         V = Dpa * pa - Dpb * pb
         
         #print(V.shape)      
