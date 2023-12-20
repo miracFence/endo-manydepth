7d33240f0fd5b53fcb1826c6a4aac490b615e4c8
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 02:58:56 -0600

    top_left_flat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -704,6 +704,7 @@ class Trainer_Monodepth2:
         pa_tr = torch.matmul(K_inv[:, :3, :3],top_right_flat.permute(0,2,1).to(device=K_inv.device))
         pb_bl = torch.matmul(K_inv[:, :3, :3],bottom_left_flat.permute(0,2,1).to(device=K_inv.device))"""
         print(D.shape)
+        print(top_left_flat.shape)
         top_left_depth = top_left_flat.to(device=K_inv.device) * D.unsqueeze(-1)
         bottom_right_depth = bottom_right_flat.to(device=K_inv.device) * D.unsqueeze(-1)
         top_right_depth = top_right_flat.to(device=K_inv.device) * D.unsqueeze(-1)
