13a81245534f0ac4d6f8937c5e3d4383ca55cb4b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 01:51:05 -0600

    orth_loss

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -702,35 +702,22 @@ class Trainer_Monodepth2:
 
         pa_tr = torch.matmul(K_inv[:, :3, :3],top_right_flat.permute(0,2,1).to(device=K_inv.device))
         pb_bl = torch.matmul(K_inv[:, :3, :3],bottom_left_flat.permute(0,2,1).to(device=K_inv.device))
-        #print(pa.shape)
-        #print(pb.shape)
-        # Use depth information to adjust positions
-        #print(top_left_flat.shape)
-        print(D.shape)
-        print(top_left_flat[:,:,:2].shape)
-        #.view(depth.size(0), 1, depth.size(2), depth.size(3))
+
         top_left_depth = top_left_flat[:,:,:2].transpose(1, 2).view(batch_size,2,height,width).to(device=K_inv.device) * D
         bottom_right_depth = bottom_right_flat[:,:,:2].transpose(1, 2).view(batch_size,2,height,width).to(device=K_inv.device) * D
         top_right_depth = top_right_flat[:,:,:2].transpose(1, 2).view(batch_size,2,height,width).to(device=K_inv.device) * D
         bottom_left_depth = bottom_left_flat[:,:,:2].transpose(1, 2).view(batch_size,2,height,width).to(device=K_inv.device) * D
 
-        """
-        top_left_depth = top_left_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D
-        bottom_right_depth = bottom_right_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D
-        top_right_depth = top_right_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D
-        bottom_left_depth = bottom_left_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D"""
-        
         top_left_depth = torch.cat([top_left_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
         bottom_right_depth = torch.cat([bottom_right_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
         top_right_depth = torch.cat([top_right_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
         bottom_left_depth = torch.cat([bottom_left_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
-        #print(top_left_depth.shape)
-        #print(pa_tl.shape)
+
         V = top_left_depth * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
         V += top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width)
 
         orth_loss = torch.einsum('bijk,bijk->bijk', N_hat, V.view(batch_size,3,height, width))
-        return orth_loss.sum()
+        return -orth_loss.sum()
 
     
     def get_ilumination_invariant_loss(self, pred, target):
