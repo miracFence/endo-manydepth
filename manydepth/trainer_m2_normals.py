3595f1036e70f448bb6a7bf3a993afb9452ad81e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 02:31:18 -0600

    top_left_depth

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -708,8 +708,11 @@ class Trainer_Monodepth2:
         top_right_depth = top_right_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D
         bottom_left_depth = bottom_left_flat.permute(0,2,1).view(batch_size,3,height,width).to(device=K_inv.device) * D
 
+        # Construct a new depth image using the mean of x and y coordinates
+        top_left_depth = (top_left_depth_reshaped[:, :, :, 0] + top_left_depth_reshaped[:, :, :, 1]) / 2
+        print(top_left_depth.shape) 
         V = top_left_depth * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
-        #V += torch.abs(top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width))
+        V += torch.abs(top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width))
 
         orth_loss = torch.einsum('bijk,bijk->bijk', N_hat, V.view(batch_size,3,height, width))
         return orth_loss.sum()
