23cb73401d44c06b729bfd6edab163e41b8973ac
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 02:40:37 -0600

    top_left_depth

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -710,11 +710,17 @@ class Trainer_Monodepth2:
 
         # Construct a new depth image using the mean of x and y coordinates
         top_left_depth = top_left_depth.view(batch_size,3,-1)
-        new_depth_image = (top_left_depth[:, 0, :] + top_left_depth[:, 1, :]) / 2
-        print(new_depth_image.shape) 
+        top_left_depth = ((top_left_depth[:, 0, :] + top_left_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
+        bottom_right_depth = bottom_right_depth.view(batch_size,3,-1)
+        bottom_right_depth = ((bottom_right_depth[:, 0, :] + bottom_right_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
+        top_right_depth = top_right_depth.view(batch_size,3,-1)
+        top_right_depth = ((top_right_depth[:, 0, :] + top_right_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
+        bottom_left_depth = bottom_left_depth.view(batch_size,3,-1)
+        bottom_left_depth = ((bottom_left_depth[:, 0, :] + bottom_left_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
+        #print(top_left_depth.shape) 
 
-        V = top_left_depth * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
-        V += torch.abs(top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width))
+        V = top_left_depth.view(batch_size,1,height,width) * pa_tl.view(batch_size,3,height,width) - bottom_right_depth.view(batch_size,1,height,width) * pb_br.view(batch_size,3,height,width)
+        V += torch.abs(top_right_depth.view(batch_size,1,height,width) * pa_tr.view(batch_size,3,height,width) - bottom_left_depth.view(batch_size,1,height,width) * pb_bl.view(batch_size,3,height,width))
 
         orth_loss = torch.einsum('bijk,bijk->bijk', N_hat, V.view(batch_size,3,height, width))
         return orth_loss.sum()
