942dd30343c10ed3d37beccb79e38417774810d6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 00:26:17 -0600

    0.5

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -750,7 +750,7 @@ class Trainer_Monodepth2:
 
         
         total_loss /= self.num_scales
-        total_loss += 13 * self.compute_orth_loss2(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
+        total_loss += 0.5 * self.compute_orth_loss2(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
         losses["loss"] = total_loss
         
         return losses
