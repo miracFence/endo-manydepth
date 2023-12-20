23ef053f9a068d671c168f4b1aff5dc70114ddfc
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 03:25:30 -0600

    depth

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -739,9 +739,8 @@ class Trainer_Monodepth2:
         bottom_right_depth = torch.cat([bottom_right_depth, ones], dim=1)
         top_right_depth = torch.cat([top_right_depth, ones], dim=1)
         bottom_left_depth = torch.cat([bottom_left_depth, ones], dim=1)
-        print(top_left_depth.shape)
 
-        V = top_left_depth.view(batch_size,1,height,width) * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
+        V = top_left_depth.view(batch_size,3,height,width) * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
         V +=  top_right_depth.view(batch_size,3,height,width) * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width)
 
         orth_loss = torch.einsum('bijk,bijk->bijk', N_hat, V.view(batch_size,3,height, width))
