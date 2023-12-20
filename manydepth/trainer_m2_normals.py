fc80af606690a95e4a69d789cab19dd8da54340a
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 01:57:18 -0600

    N_hat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -679,6 +679,7 @@ class Trainer_Monodepth2:
         _, D = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
         batch_size,channels, height, width  = D.shape
         # Create coordinate grids
+        N_hat = torch.nn.functional.normalize(N_hat, dim=1)
         y, x = torch.meshgrid(torch.arange(0, height), torch.arange(0, width))
         y = y.float().unsqueeze(0).unsqueeze(0)
         x = x.float().unsqueeze(0).unsqueeze(0)
@@ -713,8 +714,8 @@ class Trainer_Monodepth2:
         top_right_depth = torch.cat([top_right_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
         bottom_left_depth = torch.cat([bottom_left_depth,torch.ones(batch_size, 1, height , width).to(device=K_inv.device)],1)
 
-        V = torch.abs(top_left_depth * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width))
-        V += torch.abs(top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width))
+        V = top_left_depth * pa_tl.view(batch_size,3,height,width) - bottom_right_depth * pb_br.view(batch_size,3,height,width)
+        V += top_right_depth * pa_tr.view(batch_size,3,height,width) - bottom_left_depth * pb_bl.view(batch_size,3,height,width)
 
         orth_loss = torch.einsum('bijk,bijk->bijk', N_hat, V.view(batch_size,3,height, width))
         return -orth_loss.sum()
