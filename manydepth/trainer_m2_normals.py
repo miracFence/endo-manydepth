302ba6efe1b9cef48a192b9a83903948463c5604
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 18:59:27 -0600

    compute_orth_loss4

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -607,16 +607,22 @@ class Trainer_Monodepth2:
         # Calculate positions of top-left, bottom-right, top-right, and bottom-left pixels
         normal = torch.stack([x , y ], dim=-1).to(device=K_inv.device)
         right = torch.stack([x + 0.5, y ], dim=-1).to(device=K_inv.device)
-        right_right = torch.stack([x + 0.5, y], dim=-1).to(device=K_inv.device)
+        right_right = torch.stack([x + 1.0, y], dim=-1).to(device=K_inv.device)
         bottom = torch.stack([x , y + 0.5], dim=-1).to(device=K_inv.device)
-        bottom_bottom = torch.stack([x , y + 0.5], dim=-1).to(device=K_inv.device)
+        bottom_bottom = torch.stack([x , y + 1.0], dim=-1).to(device=K_inv.device)
 
         normal_flat = normal.view(1, -1, 2).expand(12, -1, -1)
         right_flat = right.view(1, -1, 2).expand(12, -1, -1)
         right_right_flat = right_right.view(1, -1, 2).expand(12, -1, -1)
         bottom_flat = bottom.view(1, -1, 2).expand(12, -1, -1)
         bottom_bottom_flat = bottom_bottom.view(1, -1, 2).expand(12, -1, -1)
+
+        right_depth = D_inv[:, :, right_flat[0, :, 1].long(), right_flat[0, :, 0].long()]
+        right_right_depth = D_inv[:, :, right_right_flat[0, :, 1].long(), right_right_flat[0, :, 0].long()]
+        bottom_flat_depth = D_inv[:, :, bottom_flat[0, :, 1].long(), bottom_flat[0, :, 0].long()]
+        bottom_bottom_depth = D_inv[:, :, bottom_bottom_flat[0, :, 1].long(), bottom_bottom_flat[0, :, 0].long()]
         
+        """
         right_depth = right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         right_right_depth = right_right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         bottom_flat_depth = bottom_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
@@ -626,6 +632,7 @@ class Trainer_Monodepth2:
         right_right_depth = ((right_right_depth[:, 0, :] + right_right_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
         bottom_flat_depth = ((bottom_flat_depth[:, 0, :] + bottom_flat_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
         bottom_bottom_depth = ((bottom_bottom_depth[:, 0, :] + bottom_bottom_depth[:, 1, :]) / 2).view(batch_size,1,height,width)
+        """
 
         ones = torch.ones(12, 1, height * width).to(device=K_inv.device)
 
@@ -886,7 +893,7 @@ class Trainer_Monodepth2:
 
         
         total_loss /= self.num_scales
-        total_loss += 0.5 * self.compute_orth_loss3(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
+        total_loss += 0.5 * self.compute_orth_loss4(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
         losses["loss"] = total_loss
         
         return losses
