b862bf79ff99cac5443587f4f1637955b051b1c4
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 22:42:36 -0600

    D

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -812,7 +812,7 @@ class Trainer_Monodepth2:
         #bottom_left_depth = bottom_left_depth.view(batch_size,3,-1)
         bottom_left_depth = ((bottom_left_depth[:, 1, :] + bottom_left_depth[:, 0, :]) / 2).view(batch_size,1,height,width)
         
-        print((D - top_left_depth).sum())
+        print((D - D).sum())
         print((D - bottom_right_depth).sum())
         print((D - top_right_depth).sum())
         print((D - top_right_depth).sum())
