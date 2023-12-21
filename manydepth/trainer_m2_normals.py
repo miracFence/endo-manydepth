e6e385a55d3fccc526b171f64bd7b6efdaf0358d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-20 20:55:28 -0600

    print

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -657,7 +657,8 @@ class Trainer_Monodepth2:
             #Cpq = torch.einsum('bijk,bijk->', N_hat_normalized,X_tilde_q.view(batch_size,3,height,width))
             Cpq = torch.einsum('bik,bik->bi', N_hat_normalized.view(12, 3, -1),X_tilde_q.view(12, 3, -1))
             #print(Cpq.unsqueeze(0).shape)
-            #print(D_inv.shape)
+            print(D_inv.shape)
+            print(depths[idx].shape)
             orth_loss += torch.abs(D_inv * Cpq.unsqueeze(-1).unsqueeze(-1) - depths[idx] * Cpp.unsqueeze(-1).unsqueeze(-1))
 
 
