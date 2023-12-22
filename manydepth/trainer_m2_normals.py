5a3595c2dfa650f3788f12946df12adc6b1cd19f
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-21 22:44:44 -0600

    Cpp

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -656,7 +656,7 @@ class Trainer_Monodepth2:
             print(Cpq.shape)
             print(D_inv.shape)
             #print(depths[idx].shape)
-            orth_loss += torch.abs(torch.matmul(D_inv.view(batch_size,1,-1), Cpq) - torch.matmul(depths[idx].view(batch_size,1,-1) , Cpp))
+            orth_loss += torch.abs(torch.matmul(D_inv, Cpq.view(12, 3, 1, 1)) - torch.matmul(depths[idx].view(batch_size,1,height,width) , Cpp.view(12, 3, 1, 1)))
 
         # Compute gradient of the image
         #print(image_batch.shape)
