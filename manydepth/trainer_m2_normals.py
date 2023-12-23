799896a80c74c5c5cc51ca1cf38a2f879e826ce3
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-23 12:42:50 -0600

    bottom_right

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -806,29 +806,27 @@ class Trainer_Monodepth2:
 
         # Calculate positions of top-left, bottom-right, top-right, and bottom-left pixels
         
-        top_left = torch.stack([x - 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
-        bottom_right = torch.stack([x + 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
-        top_right = torch.stack([x + 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
-        bottom_left = torch.stack([x - 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
+        top_left = torch.stack([x - 1, y - 1], dim=-1).to(device=K_inv.device)
+        bottom_right = torch.stack([x + 1, y + 1], dim=-1).to(device=K_inv.device)
+        top_right = torch.stack([x + 1, y - 1], dim=-1).to(device=K_inv.device)
+        bottom_left = torch.stack([x - 1, y + 1], dim=-1).to(device=K_inv.device)
 
         #xy = torch.stack([x, y], dim=-1).to(device=K_inv.device)
         #xy = xy.view(1, -1, 2).expand(12, -1, -1)
         #positions_a, positions_b = self.compute_nearby_positions(xy.view(12,2,height,width))
         #print(xy[0])
         # Flatten and concatenate to get pairs of positions
+        print(top_left)
 
         top_left_flat = top_left.view(1, -1, 2).expand(12, -1, -1)
         bottom_right_flat = bottom_right.view(1, -1, 2).expand(12, -1, -1)
         top_right_flat = top_right.view(1, -1, 2).expand(12, -1, -1)
         bottom_left_flat = bottom_left.view(1, -1, 2).expand(12, -1, -1)
-
-        print(top_left_flat)
         
-        """
         top_left_depth = top_left_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         bottom_right_depth = bottom_right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         top_right_depth = top_right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
-        bottom_left_depth = bottom_left_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)"""
+        bottom_left_depth = bottom_left_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         
         top_left_flat = torch.cat([top_left_flat.permute(0,2,1), ones], dim=1)
         bottom_right_flat = torch.cat([bottom_right_flat.permute(0,2,1), ones], dim=1)
