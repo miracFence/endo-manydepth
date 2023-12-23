d90e27444fa8f987709ff9597a543fd7a1a97686
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-23 00:20:04 -0600

    offset_a

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -769,11 +769,9 @@ class Trainer_Monodepth2:
         #offset_a = offset_a.view(1,2, 1, 1).expand(12, -1, -1,-1)
         #offset_b = offset_b.view(1,2, 1, 1).expand(12, -1, -1,-11)
 
-        #offset_a = offset_a.view(1, 2, 2, 1)
-        #offset_b = offset_b.view(1, 2, 2, 1)
-        
-        offset_a = offset_a.view(1, 2, 1, 1)
-        offset_b = offset_b.view(1, 2, 1, 1)
+          # Reshape the offset tensors to (1, 2, 1, 1)
+        offset_a = offset_a.view(1, 2, 1, 2)
+        offset_b = offset_b.view(1, 2, 1, 2)
 
         # Compute positions_a and positions_b
         print(pixel_positions.shape)
