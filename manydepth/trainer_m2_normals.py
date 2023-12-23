9130d5367f99a44eb66818bfb0158d35536ff3df
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-23 10:00:12 -0600

    bottom_right_flat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -805,11 +805,11 @@ class Trainer_Monodepth2:
         N_hat_normalized = N_hat / magnitude
 
         # Calculate positions of top-left, bottom-right, top-right, and bottom-left pixels
-        """
+        
         top_left = torch.stack([x - 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
         bottom_right = torch.stack([x + 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
         top_right = torch.stack([x + 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
-        bottom_left = torch.stack([x - 0.5, y + 0.5], dim=-1).to(device=K_inv.device)"""
+        bottom_left = torch.stack([x - 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
 
         #xy = torch.stack([x, y], dim=-1).to(device=K_inv.device)
         #xy = xy.view(1, -1, 2).expand(12, -1, -1)
