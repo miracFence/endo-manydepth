d66604267c9699da53634267604b63508bbaa0ed
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-26 12:36:10 -0600

    transpose

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -834,14 +834,14 @@ class Trainer_Monodepth2:
 
 
         #V = (top_left_depth.view(12,1,height,width) * pa_tl.view(12,3,height,width)) - (bottom_right_depth.view(12,1,height,width) * pb_br.view(12,3,height,width))
-        V = torch.matmul(top_left_depth.view(12, height * width, 1) , pa_tl.view(12, 3, height * width)) - torch.matmul(bottom_right_depth.view(12, height * width, 1) , pb_br.view(12, 3, height * width))
+        V = torch.bmm(top_left_depth.view(12, height * width, 1) , pa_tl.view(12, 3, height * width).transpose(1, 2)) - torch.bmm(bottom_right_depth.view(12, height * width, 1) , pb_br.view(12, 3, height * width).transpose(1, 2))
         orth_loss1 = torch.sum(torch.einsum('bijk,bijk->bi', V.view(batch_size,height,width,3),N_hat_normalized.view(batch_size,height,width,3)))
         #orth_loss1 = V.view(12,height,width,3) * N_hat_normalized.view(12,height,width,3)
         # Sum over the channel dimension (dimension 1)
         #orth_loss1 = orth_loss1.sum(dim=-1)
 
         #V = (top_right_depth.view(12,1,height,width) * pa_tr.view(12,3,height,width)) - (bottom_left_depth.view(12,1,height,width) * pb_bl.view(12,3,height,width))
-        V = torch.matmul(top_right_depth.view(12, height * width, 1) , pa_tr.view(12, 3, height * width)) - torch.matmul(bottom_left_depth.view(12, height * width, 1) , pb_bl.view(12, 3, height * width))
+        V = torch.bmm(top_right_depth.view(12, height * width, 1) , pa_tr.view(12, 3, height * width).transpose(1, 2)) - torch.bmm(bottom_left_depth.view(12, height * width, 1) , pb_bl.view(12, 3, height * width).transpose(1, 2))
         orth_loss2 = torch.sum(torch.einsum('bijk,bijk->bi', V.view(batch_size,height,width,3),N_hat_normalized.view(batch_size,height,width,3)))
         #orth_loss2 = V.view(12,height,width,3) * N_hat_normalized.view(12,height,width,3)
         # Sum over the channel dimension (dimension 1)
