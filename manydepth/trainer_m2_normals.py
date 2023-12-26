baac53763f5e81555de374415f5915f7723d6e03
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-26 15:32:33 -0600

    top_left_flat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -798,7 +798,7 @@ class Trainer_Monodepth2:
         bottom_left_depth = bottom_left_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         """
         
-        print(top_left_depth)
+        print(top_left_flat)
         top_left_depth = D[:, :, top_left_flat[0,:,1].long(), top_left_flat[0,:,0].long()]
         bottom_right_depth = D[:, :, bottom_right_flat[0,:,1].long(), bottom_right_flat[0,:,0].long()]
         top_right_depth = D[:, :, top_right_flat[0,:,1].long(), top_right_flat[0,:,0].long()]
