32575bf2d85f373b612ba9c0884ad2db13663a77
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-26 15:35:00 -0600

    print(top_left_flat)

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -773,10 +773,10 @@ class Trainer_Monodepth2:
 
         # Calculate positions of top-left, bottom-right, top-right, and bottom-left pixels
         
-        top_left = torch.stack([x - 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
-        bottom_right = torch.stack([x + 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
-        top_right = torch.stack([x + 0.5, y - 0.5], dim=-1).to(device=K_inv.device)
-        bottom_left = torch.stack([x - 0.5, y + 0.5], dim=-1).to(device=K_inv.device)
+        top_left = torch.stack([y - 0.5, x - 0.5], dim=-1).to(device=K_inv.device)
+        bottom_right = torch.stack([y + 0.5, x + 0.5], dim=-1).to(device=K_inv.device)
+        top_right = torch.stack([y + 0.5, x - 0.5], dim=-1).to(device=K_inv.device)
+        bottom_left = torch.stack([y - 0.5, x + 0.5], dim=-1).to(device=K_inv.device)
 
         #xy = torch.stack([x, y], dim=-1).to(device=K_inv.device)
         #xy = xy.view(1, -1, 2).expand(12, -1, -1)
