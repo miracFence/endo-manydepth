c6aa11cb62cff8c1c59f3c2d8b42d472354fdfcc
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-28 21:57:36 -0600

    top_left

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -780,7 +780,8 @@ class Trainer_Monodepth2:
         bottom_right = torch.stack([torch.clamp(y - 1.0, min=0, max=height-1), torch.clamp(x - 1.0, min=0, max=width-1)], dim=-1).to(device=K_inv.device)
         top_right = torch.stack([torch.clamp(y + 1.0, min=0, max=height-1), torch.clamp(x - 1.0, min=0, max=width-1)], dim=-1).to(device=K_inv.device)
         bottom_left = torch.stack([torch.clamp(y - 1.0, min=0, max=height-1), torch.clamp(x + 1.0, min=0, max=width-1)], dim=-1).to(device=K_inv.device)
-
+        print(top_left.shape)
+        print(top_left)
         """    
         top_left = torch.stack([torch.clamp(x + 1.0, min=0, max=width-1), torch.clamp(y + 1.0, min=0, max=height-1)], dim=-1).to(device=K_inv.device)
         bottom_right = torch.stack([torch.clamp(x - 1.0, min=0, max=width-1), torch.clamp(y - 1.0, min=0, max=height-1)], dim=-1).to(device=K_inv.device)
