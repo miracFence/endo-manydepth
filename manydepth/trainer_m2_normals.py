47c00bde635240f4267118ba5faf2a3db962ac36
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2023-12-30 11:37:48 -0600

    shape

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -818,7 +818,7 @@ class Trainer_Monodepth2:
         bottom_right_depth = bottom_right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         top_right_depth = top_right_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
         bottom_left_depth = bottom_left_flat.permute(0, 2, 1).to(device=K_inv.device) * D.view(batch_size, 1, -1)
-        
+        print(top_left_depth.shape)
         D_hat_pa = torch.nn.functional.grid_sample(D, top_left_depth.view(batch_size, 1, -1, 2), mode='bilinear', align_corners=False)
         print(D_hat_pa)
         #D = D.permute(0,2,3,1)
