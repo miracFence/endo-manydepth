cdd53761d04f8487b387224be1c5c9c1b51a227c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-02 10:34:36 -0600

    top_left_flat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -866,10 +866,10 @@ class Trainer_Monodepth2:
 
         #print(top_left_depth.shape)
 
-        top_left_flat = torch.cat([top_left_flat.permute(0,2,1), ones], dim=1)
-        bottom_right_flat = torch.cat([bottom_right_flat.permute(0,2,1), ones], dim=1)
-        top_right_flat = torch.cat([top_right_flat.permute(0,2,1), ones], dim=1)
-        bottom_left_flat = torch.cat([bottom_left_flat.permute(0,2,1), ones], dim=1)
+        top_left_flat = torch.cat([top_left_flat, ones], dim=1)
+        bottom_right_flat = torch.cat([bottom_right_flat, ones], dim=1)
+        top_right_flat = torch.cat([top_right_flat, ones], dim=1)
+        bottom_left_flat = torch.cat([bottom_left_flat, ones], dim=1)
 
         #print(top_left_flat.shape)
 
