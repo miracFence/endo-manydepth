02f2a5e81a92eb3235e6e0fc9f661762455d29e8
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-02 19:26:37 -0600

    top_left_flat

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -811,10 +811,11 @@ class Trainer_Monodepth2:
         top_right_flat = top_right.view(1,-1, 2).expand(12, -1, -1)
         bottom_left_flat = bottom_left.view(1,-1, 2).expand(12, -1, -1)
 
+        """
         top_left_flat_ = D.view(batch_size, 1, -1) * top_left_flat.permute(0,2,1)
         bottom_right_flat_ = D.view(batch_size, 1, -1) * bottom_right_flat.permute(0,2,1)
         top_right_flat_ = D.view(batch_size, 1, -1) * top_right_flat.permute(0,2,1)
-        bottom_left_flat_ = D.view(batch_size, 1, -1) * bottom_left_flat.permute(0,2,1)
+        bottom_left_flat_ = D.view(batch_size, 1, -1) * bottom_left_flat.permute(0,2,1)"""
       
         """
         top_left_flat_ = top_left_flat.view(batch_size,2,height,width).clone()
@@ -866,10 +867,10 @@ class Trainer_Monodepth2:
 
         #print(top_left_depth.shape)
 
-        top_left_flat = torch.cat([top_left_flat_, ones], dim=1)
-        bottom_right_flat = torch.cat([bottom_right_flat_, ones], dim=1)
-        top_right_flat = torch.cat([top_right_flat_, ones], dim=1)
-        bottom_left_flat = torch.cat([bottom_left_flat_, ones], dim=1)
+        top_left_flat = torch.cat([top_left_flat.permute(0,2,1), ones], dim=1)
+        bottom_right_flat = torch.cat([bottom_right_flat.permute(0,2,1), ones], dim=1)
+        top_right_flat = torch.cat([top_right_flat.permute(0,2,1), ones], dim=1)
+        bottom_left_flat = torch.cat([bottom_left_flat.permute(0,2,1), ones], dim=1)
 
         #print(top_left_flat.shape)
 
