ebb6bd02ca0591661d14830993726574db7899c3
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-04 08:25:09 -0600

    MySfMLearner4

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -23,7 +23,7 @@ from torch.utils.data import DataLoader
 import wandb
 
 
-wandb.init(project="MySfMLearner3", entity="respinosa")
+wandb.init(project="MySfMLearner4", entity="respinosa")
 
 
 import json
