97e3cdb3716041e7af29207ee445436e2c2c994c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-05 11:19:40 -0600

    print

--- manydepth/export_gt_depth2.py
+++ manydepth/export_gt_depth2.py
@@ -51,7 +51,7 @@ def export_gt_depths_kitti():
                                          "groundtruth", "image_02", "{:010d}.png".format(frame_id))
         elif opt.split == "RNNSLAM":
             gt_depth_path = os.path.join(opt.data_path, folder, "{:010d}.png".format(frame_id))
-            print(gt_depth_path.shape)
+            print(gt_depth_path)
             gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256
 
         gt_depths.append(gt_depth.astype(np.float32))
