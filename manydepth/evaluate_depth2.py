3bf0d0fc0c22aceef033ee9463755cd9506bf7b5
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-05 20:53:35 -0600

    input_color

--- manydepth/evaluate_depth2.py
+++ manydepth/evaluate_depth2.py
@@ -116,7 +116,7 @@ def evaluate(opt):
         with torch.no_grad():
             for data in dataloader:
                 input_color = data[("color", 0, 0)].cuda()
-
+                print(input_color.shape)
                 if opt.post_process:
                     # Post-processed results require each image to have two forward passes
                     input_color = torch.cat((input_color, torch.flip(input_color, [3])), 0)
