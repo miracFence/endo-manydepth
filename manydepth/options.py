611a4c836ec0d28d273f4ab28efefe31f9414de9
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-09 10:01:50 -0600

    illumination_invariant

--- manydepth/options.py
+++ manydepth/options.py
@@ -74,7 +74,7 @@ class MonodepthOptions:
         self.parser.add_argument("--illumination_invariant",
                                  type=float,
                                  help="illumination invariant weight",
-                                 default=0.5)
+                                 default=0.3)
         self.parser.add_argument("--orthogonal",
                                  type=float,
                                  help="orthogonal weight",
