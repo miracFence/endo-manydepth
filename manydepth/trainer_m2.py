568de3a07103e570ac5e3280e33a40eb6dcb8ced
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-09 21:44:51 -0600

    color_refined

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -137,7 +137,7 @@ class Trainer_Monodepth:
         print("Models and tensorboard events files are saved to:\n  ", self.opt.log_dir)
         print("Training is using:\n  ", self.device)
 
-        # DATA
+                # DATA
         datasets_dict = {"kitti": datasets.KITTIRAWDataset,
                          "cityscapes_preprocessed": datasets.CityscapesPreprocessedDataset,
                          "kitti_odom": datasets.KITTIOdomDataset,
@@ -700,7 +700,6 @@ class Trainer_Monodepth:
 
             disp = outputs[("disp", scale)]
             color = inputs[("color", 0, scale)]
-
             #Losses & compute mask
             for frame_id in self.opt.frame_ids[1:]:
                 # Mask
