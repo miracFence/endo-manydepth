b6acd60e1cada5abf13c5f9b34286fb613257b82
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-10 09:10:57 -0600

    K

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -34,8 +34,8 @@ class SCAREDDataset(MonoDataset):
         #Colon10k dataset
         #256 / 320
         #Camera Intrinsics: Pinhole fx=145.4410 fy=145.4410 cx=135.6993 cy=107.8946 width=270 height=216
-        self.K = np.array([[0.454503125, 0, 0.4240603125, 0],
-                           [0, 0.56812890625,0.42146328125, 0],
+        self.K = np.array([[0.53867, 0, 0.50259, 0],
+                           [0, 0.673337,0.499512, 0],
                            [0, 0, 1, 0],
                            [0, 0, 0, 1]], dtype=np.float32)
 
