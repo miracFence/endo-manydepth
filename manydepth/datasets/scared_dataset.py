11db5b7dda4b63d503cdb77e28cf31d59113a955
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-13 11:41:30 -0600

    SCAREDDataset

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -13,10 +13,10 @@ class SCAREDDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
         super(SCAREDDataset, self).__init__(*args, **kwargs)
         #SCARED Dataset
-        """self.K = np.array([[0.82, 0, 0.5, 0],
+        self.K = np.array([[0.82, 0, 0.5, 0],
                            [0, 1.02, 0.5, 0],
                            [0, 0, 1, 0],
-                           [0, 0, 0, 1]], dtype=np.float32)"""
+                           [0, 0, 0, 1]], dtype=np.float32)
                 
         #256 / 320
         #fx769.807403688120 fy769.720558534159 cx675.226397736271 cy548.903474592445 k1-0.454260397098776 k20.179156666748519 k3-0.0285017743214105 p1-0.00134889190333418 p20.000738912923806121 skew-0.141152521412316
@@ -28,10 +28,10 @@ class SCAREDDataset(MonoDataset):
         #256 / 320
         #fx = 155.00297855750486 fy= 165.3365104613385 cx = 160.0; cy = 128.0;
         #fy = 176.358213824 cy =  136.533333333
-        self.K = np.array([[0.48, 0, 0.5, 0],
+        """self.K = np.array([[0.48, 0, 0.5, 0],
                            [0, 0.6888,0.5333, 0],
                            [0, 0, 1, 0],
-                           [0, 0, 0, 1]], dtype=np.float32)
+                           [0, 0, 0, 1]], dtype=np.float32)"""
         #Colon10k dataset
         #256 / 320
         #Camera Intrinsics: Pinhole fx=145.4410 fy=145.4410 cx=135.6993 cy=107.8946 width=270 height=216
@@ -83,11 +83,11 @@ class SCAREDDataset(MonoDataset):
 
     def get_image_path(self, folder, frame_index, side):
         #SCATER
-        #f_str = "{}{}".format(frame_index, self.img_ext)
-        #image_path = os.path.join(self.data_path, folder, "data", f_str)
+        f_str = "{}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(self.data_path, folder, "data", f_str)
         #COLON10k
-        f_str=str(frame_index) + self.img_ext
-        image_path = os.path.join(self.data_path, folder, f_str)
+        #f_str=str(frame_index) + self.img_ext
+        #image_path = os.path.join(self.data_path, folder, f_str)
             
         return image_path
 
@@ -97,11 +97,11 @@ class SCAREDRAWDataset(SCAREDDataset):
 
     def get_image_path(self, folder, frame_index, side):
         #SCATER
-        #f_str = "{}{}".format(frame_index, self.img_ext)
-        #image_path = os.path.join(self.data_path, folder, "data", f_str)
+        f_str = "{}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(self.data_path, folder, "data", f_str)
         #COLON10k
-        f_str=str(frame_index) + self.img_ext
-        image_path = os.path.join(self.data_path, folder, f_str)
+        #f_str=str(frame_index) + self.img_ext
+        #image_path = os.path.join(self.data_path, folder, f_str)
             
         return image_path
 
