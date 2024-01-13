e8b2430b501d9dec7c34834d2275bb067a69746f
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-12 22:04:40 -0600

    color_refined

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -523,7 +523,11 @@ class Trainer_Monodepth:
                 #if scale == 0:
                 #outputs["c_"+str(frame_id)] = outputs["c_"+str(frame_id)] * reprojection_loss_mask 
                 #outputs["b_"+str(frame_id)] = outputs["b_"+str(frame_id)] * reprojection_loss_mask
-                outputs[("color_refined", frame_id)] = outputs[("color_refined", frame_id)] * reprojection_loss_mask
+
+                #outputs[("refined", scale, f_i)] = (outputs[("transform", "high", scale, f_i)] * outputs[("occu_mask_backward", 0, f_i)].detach()  + inputs[("color", 0, 0)])
+                #outputs[("refined", scale, f_i)] = torch.clamp(outputs[("refined", scale, f_i)], min=0.0, max=1.0)
+                outputs[("color_refined", frame_id)] = outputs[("color_refined", frame_id)] * reprojection_loss_mask  + inputs[("color", 0, 0)]
+                outputs[("color_refined", frame_id)] = torch.clamp(outputs[("color_refined", frame_id)], min=0.0, max=1.0)
                 #Losses
                 target = outputs[("color_refined", frame_id)] #Lighting
                 pred = outputs[("color", frame_id, scale)]
