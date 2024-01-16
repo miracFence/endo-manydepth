20bfd70199fb7189fbf40bdd1034db240658ba9c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-15 19:31:07 -0600

    max_depth

--- manydepth/options.py
+++ manydepth/options.py
@@ -95,7 +95,8 @@ class MonodepthOptions:
         self.parser.add_argument("--max_depth",
                                  type=float,
                                  help="maximum depth",
-                                 default=100.0)
+                                 default=150.0)
+
         self.parser.add_argument("--frame_ids",
                                  nargs="+",
                                  type=int,
