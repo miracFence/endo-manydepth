ea8f57188ca0ec30aa11228dc74aee62891fbc5d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-17 21:32:02 -0600

    DepthDecoderT

--- manydepth/networks/__init__.py
+++ manydepth/networks/__init__.py
@@ -1,5 +1,6 @@
 from .resnet_encoder import ResnetEncoder, ResnetEncoderMatching
 from .depth_decoder import DepthDecoder
+from .hr_decoder import DepthDecoderT
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .lighting_decoder import LightingDecoder
