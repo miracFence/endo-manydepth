5f134e41fb93b6a12fe943c92eb7646e64086605
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-17 22:28:52 -0600

    DepthDecoderT

--- manydepth/networks/hr_decoder.py
+++ manydepth/networks/hr_decoder.py
@@ -7,9 +7,9 @@ from collections import OrderedDict
 from .hr_layers import *
 
 
-class DepthDecoder(nn.Module):
+class DepthDecoderT(nn.Module):
     def __init__(self, ch_enc = [64,128,256,512,512], scales=range(4),num_ch_enc = [ 64, 64, 128, 256, 512 ], num_output_channels=1):
-        super(DepthDecoder, self).__init__()
+        super(DepthDecoderT, self).__init__()
         self.num_output_channels = num_output_channels
         self.num_ch_enc = num_ch_enc
         self.ch_enc = ch_enc
