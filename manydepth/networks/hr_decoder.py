a102edea04bf508b3e31f94480ce1262ea2eb428
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-17 21:49:22 -0600

    DepthDecoderT

--- manydepth/networks/hr_decoder.py
+++ manydepth/networks/hr_decoder.py
@@ -8,7 +8,7 @@ from .hr_layers import *
 
 
 class DepthDecoderT(nn.Module):
-    def __init__(self, ch_enc = [64,128,216,288,288], scales=range(4),num_ch_enc = [ 64, 64, 128, 256, 512 ], num_output_channels=1):
+    def __init__(self, ch_enc, scales=range(4),num_output_channels=1):
         super(DepthDecoderT, self).__init__()
         self.num_output_channels = num_output_channels
         self.num_ch_enc = num_ch_enc
