79042e70df5903e60a4c9d3c3d25fea3600c6ff3
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-19 11:20:43 -0600

    mpvit_small

--- manydepth/networks/mpvit.py
+++ manydepth/networks/mpvit.py
@@ -812,7 +812,7 @@ def mpvit_small(**kwargs):
         num_heads=[8, 8, 8, 8],
         **kwargs,
     )
-    checkpoint = torch.load('./ckpt/mpvit_small.pth', map_location=lambda storage, loc: storage)['model']
+    checkpoint = torch.load('/workspace/endo-manydepth/manydepth/mpvit_small.pth', map_location=lambda storage, loc: storage)['model']
     #logger = get_root_logger()
     #load_state_dict(model, checkpoint, strict=False, logger=logger)
     del checkpoint
