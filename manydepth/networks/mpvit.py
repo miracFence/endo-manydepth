20c780409350c97986dcf1ffe0df78db70f90800
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-22 21:04:27 -0600

    BACKBONES

--- manydepth/networks/mpvit.py
+++ manydepth/networks/mpvit.py
@@ -35,7 +35,7 @@ from mmcv.utils import get_logger
 #mmdet.utils.logger
 #from mmcv.utils import get_logger
 #from mmdet.utils import get_root_logger
-#from mmseg.models.builder import BACKBONES
+from mmseg.models.builder import BACKBONES
 
 __all__ = [
     "mpvit_tiny",
@@ -604,7 +604,7 @@ def dpr_generator(drop_path_rate, num_layers, num_stages):
     return dpr
 
 
-#@BACKBONES.register_module()
+@BACKBONES.register_module()
 class MPViT(nn.Module):
     """Multi-Path ViT class."""
 
