0e5aecd6c2889b9bb135187d7cb09fdfae77842d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-01-23 10:30:38 -0600

    image_path

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -86,6 +86,7 @@ class SCAREDDataset(MonoDataset):
         f_str = "{}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(self.data_path, folder, "data", f_str)
         #COLON10k
+        
         #f_str=str(frame_index) + self.img_ext
         #image_path = os.path.join(self.data_path, folder, f_str)
             
